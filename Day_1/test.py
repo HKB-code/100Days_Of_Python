11a0ca197a703481633453b69dc1a285447f70b8
HKB-code <harshilj2002@gmail.com>
2025-05-26 19:40:22 +0530

    Day-1

---
+++ Day_1/test.py
@@ -0,0 +1,8 @@
+print("hello")
+print("hello\nhi")
+print("hello" + " "+ "hi")
+
+# input("a prompt for user,as what kind of data we wnat")
+print("hello " +input("What is your Nmae?") + "!")
+name  = input("What is your name?")
+print(len(name))
