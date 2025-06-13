20651c12a7d7fc8f6350b4211bf76339ec9836ff
HKB-code <harshilj2002@gmail.com>
2025-06-13 16:00:40 +0530

    Day_10

---
+++ Day_10/project.py
@@ -0,0 +1,53 @@
+logo=r'''
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ '.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ '.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   '._____.'  | || ||____|  |____|| || |  |________|  | || |   '._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|  
+'''
+def add(n1,n2):
+    return n1+n2
+def sub(n1,n2):
+    return n1-n2
+def mul(n1,n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
+
+operations = {
+    "*": mul,
+    "+": add,
+    "-":sub,
+    "/":div
+}
+should_continue = True
+def calculate():
+  global should_continue
+  print(logo)
+  n1 = int(input("what is the first digit"))
+  while should_continue: 
+   for operation in operations:
+    print(operation)
+   opeartor = input("Pick an operation?")
+   n2 = int(input("what is the second digit"))
+   ans = operations[opeartor](n1,n2)
+   repeat= input(f"Type 'y' to continue calculating with {ans}, or type 'n' to start a new calculation:").lower()
+   if repeat =='y':
+     n1 = ans
+   else:
+      should_continue = False
+      print(f"here is the {ans}")
+      calculate() 
+
+
+calculate()
+
